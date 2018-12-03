98d2c7ccc0c53ebb774cfbe10d540892398ee093
Antoine Awaida <antoine.awaida@student.ecp.fr>
2018-12-03 16:35:02 +0100

    test

--- controllers/groups/groups.controller.js
+++ controllers/groups/groups.controller.js
@@ -1,4 +1,5 @@
 const GroupModel = require('../../models/group.js')
+const UserModel = require('../../models/user.js')
 
 
 exports.create = async function(req,res,err) {
@@ -16,6 +17,16 @@ exports.getAll = async function(req,res,err) {
 
   GroupModel.find((err,groups) => {
     if (err) return next(err);
+    let users = [];
+    groups.forEach(function(e){
+      e.members.forEach(function(member){
+        UserModel.findById(member,function(err,user){
+          users.push(user);
+        })
+      })
+      
+    })
+    console.log(users)
     res.json(groups);
   })
 
