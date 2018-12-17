12f5bb1ea7b212dbd82fb7ced7793882dd01bcaa
Arié Selinger <arieselinger@arie.local>
2018-12-17 17:40:10 +0100

    Get une tâche d'un user

--- controllers/tasks/tasks.controller.js
+++ controllers/tasks/tasks.controller.js
@@ -5,53 +5,53 @@ const UserModel = require('../../models/user');
 
 
 exports.create = async function (req, res, err) {
-  try{
- // Créer la tâche
-  let task = req.body;
-  task.status = 'pending';
-  let createdTask = new TaskModel(task);
+  try {
+    // Créer la tâche
+    let task = req.body;
+    task.status = 'pending';
+    let createdTask = new TaskModel(task);
 
-  // Sauvegarder la tâche
-  await createdTask.save()
+    // Sauvegarder la tâche
+    await createdTask.save()
 
-  // Ajout de la tâche à chaque groupe.
+    // Ajout de la tâche à chaque groupe.
 
     let taskWithGroups = await TaskModel.findById(createdTask._id).populate('groups');
     let groups = taskWithGroups.groups;
-    for(let i=0; i<groups.length; i++){
+    for (let i = 0; i < groups.length; i++) {
       let group = groups[i];
       group.tasks.push(createdTask._id);
-      await GroupModel.findByIdAndUpdate(group._id, {tasks : group.tasks});
+      await GroupModel.findByIdAndUpdate(group._id, { tasks: group.tasks });
     }
-  
-  }catch(e){
+
+  } catch (e) {
     return res.status(500).json("Impossible de créer la tâche");
   }
   return res.status(200).json("La tâche a bien été créé!");
 }
 
-exports.getTasksFromUser = async function(req, res, err){
+exports.getTasksFromUser = async function (req, res, err) {
 
-  try{
+  try {
     const userID = req.params.userID;
-    let groups = await GroupModel.find({members : {$in : userID}}).populate('tasks').populate('author'); // Les groupes du User
+    let groups = await GroupModel.find({ members: { $in: userID } }).populate('tasks').populate('author'); // Les groupes du User
     let tasks = [];
     //console.log(groups);
-    for(let i=0; i<groups.length; i++){
+    for (let i = 0; i < groups.length; i++) {
       let group = groups[i];
-      for(let j=0; j<group.tasks.length; j++){
+      for (let j = 0; j < group.tasks.length; j++) {
         let task = group.tasks[j];
 
         //Avant d'ajouter la tâche on vérifie qu'elle n'est pas dedans
         let isAlready = false;
-        for(var k=0; k<tasks.length; k++){
+        for (var k = 0; k < tasks.length; k++) {
           let task2 = tasks[k];
-          if (task2._id.equals(task._id)){
+          if (task2._id.equals(task._id)) {
             isAlready = true;
           }
         }
-        if(!isAlready){
-          task = await TaskModel.findById(task._id).populate({path : 'author', select : ['first_name', 'last_name']});
+        if (!isAlready) {
+          task = await TaskModel.findById(task._id).populate({ path: 'author', select: ['first_name', 'last_name'] });
           tasks.push(task);
         }
 
@@ -65,13 +65,42 @@ exports.getTasksFromUser = async function(req, res, err){
     // });
 
     console.log(tasks);
-    
+
     //console.log("********\n");
     //console.log(tasks);
     //console.log("********\n");
     return res.status(200).send(tasks);
-} catch(e){
-  return res.status(500).json("Impossible de récupérer les tâches du user");
+  } catch (e) {
+    return res.status(500).json("Impossible de récupérer les tâches du user");
+  }
+
 }
+
+exports.getTaskFromUser = async function (req, res, err) {
+
+  try {
+    if (!req.body.taskID) {
+      return res.status(500).json("Il manque le paramètre 'taskID'");
+    }
+    else {
+      const taskID = req.body.taskID;
+      const userID = req.params.userID;
+
+      let groups = await GroupModel.find({ members: { $in: userID } }).populate('tasks').populate('author'); // Les groupes du User
+      for (let i = 0; i < groups.length; i++) {
+        let group = groups[i];
+        for (let j = 0; j < group.tasks.length; j++) {
+          let task = group.tasks[j];
+          if (task._id.equals(taskID)) {
+            task = await TaskModel.findById(task._id).populate({ path: 'author', select: ['first_name', 'last_name'] });
+            return res.status(200).send(task);
+          }
+        }
+      }
+      return res.status(500).json("Ce user ne peut pas accéder à cette tâche");
+    }
+  } catch (e) {
+    return res.status(500).json("Impossible de récupérer les tâches du user : "+e.message);
+  }
 
 }
--- server/routes/api/tasks.route.js
+++ server/routes/api/tasks.route.js
@@ -5,6 +5,7 @@ var TasksController = require('../../../controllers/tasks/tasks.controller');
 
 router.post('/createtask/', TasksController.create);
 router.get('/gettasks/:userID', TasksController.getTasksFromUser);
+router.post('/gettask/:userID', TasksController.getTaskFromUser);
 
 
 module.exports = router;
