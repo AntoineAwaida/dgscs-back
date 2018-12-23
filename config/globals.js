eb55857a9ab64b5f5bb899f49c77cb5e7e7d1e64
Arié Selinger <arieselinger@arie.home>
2018-12-23 18:32:57 +0100

    localhost

--- config/globals.js
+++ config/globals.js
@@ -1,6 +1,5 @@
-
-//server = 'http://localhost:3000/'
-server = 'http://cs3.cs-campus.fr:3000/'
+server = 'http://localhost:3000/'
+// server = 'http://cs3.cs-campus.fr:3000/'
 
 
 module.exports = {
