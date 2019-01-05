4107f7a5e95de0bfa5a98a2168ab666e5d51bc0d
Antoine Awaida <antoine.awaida@student.ecp.fr>
2019-01-05 17:51:38 +0100

    api annonces

--- server/routes/api.route.js
+++ server/routes/api.route.js
@@ -5,10 +5,12 @@ var users = require('./api/users.route');
 var groups = require('./api/groups.route');
 var workpackages = require('./api/workpackages.route');
 var tasks = require('./api/tasks.route');
+var announces = require('./api/announces.route');
 
 router.use('/users', users);
 router.use('/groups',groups);
 router.use('/workpackages',workpackages);
 router.use('/tasks', tasks);
+router.use('/announces',announces);
 
 module.exports = router;
