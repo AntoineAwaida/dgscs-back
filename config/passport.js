8812f6649d552c98ece7d34beda2fe09104cde42
Arié Selinger <arieselinger@mstm-imp2.ens.ecp.fr>
2019-01-09 17:19:12 +0100

    select all test

--- config/passport.js
+++ config/passport.js
@@ -7,7 +7,7 @@ passport.use(new LocalStrategy({
 },
   function (username, password, done) {
     UserModel.findOne(
-      { email: username }, 'email password',
+      { email: username }, '*',
       function (err, user) {
 
         if (err) {
